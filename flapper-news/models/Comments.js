bc91fe938aefcf59154d7a59f554f6b8db2299ce
Francesco Tormene <francesco.tormene@gmail.com>
2016-02-12 14:40:40 +0100

    Creato backend node. non vanno routes parametriche
    Finito di creare modelli con mongoose e routes con express/node.
    Non riesco a far funzionare le routes paramentriche.

---
+++ flapper-news/models/Comments.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Francesco on 12/02/2016.
+ */
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+    body: String,
+    author: String,
+    upvotes: {type: Number, default: 0},
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+});
+
+CommentSchema.methods.upvote = function(cb) {
+    this.upvotes += 1;
+    this.save(cb);
+};
+
+mongoose.model('Comment', CommentSchema);
